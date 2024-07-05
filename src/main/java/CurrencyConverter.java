e7c0083f9f2962b174aadf68c90f98a7b5b88a6d
giovxna <gi.siqueira0202@gmail.com>
2024-07-05 03:15:46 -0300

    establishing connection w/ api

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -1,2 +1,11 @@
+import java.net.http.HttpClient;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CurrencyConverter {
+    private static final String API_KEY = "865314104ccf322e5d4abce6";
+    private static final String API_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/";
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static Map<String, Double> rates = new HashMap<>();
+
 }
